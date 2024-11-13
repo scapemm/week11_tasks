695a476dc6b93d757649a687c247a3724fb5b5cc
scapemm <a.shukurullayev@newuu.uz>
2024-11-13 12:36:22 +0500

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void search(string& s, char& key){
+    bool  j=true;
+    for(int i=0; i<s.size(); ++i){
+        if(s[i]==key){
+            cout << "Found\n";
+            return;
+        }
+    }
+   cout << "Not found\n";
+}
+int main() {
+   char c;
+   string s;
+   cin >> c ;
+   getline(cin,s);
+   search(s,c);
+   //cout << s;
+   //cout << c  << "\n";
+   // << s;
+   return 0;
+}
